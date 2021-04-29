741f575c40952e785cb43b2644491a84df05b8ab
Borko <bstoilov81@gmail.com>
2021-04-29 10:51:25 +0300

    Add detail movie view and css for template

--- config/core/urls.py
+++ config/core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import (
-    all_movie,
+    MovieDetail,
     MovieList,
 )
 app_name = 'movies'
 urlpatterns = [
     path('', MovieList.as_view(), name='all_movie'),
-    path('all/', all_movie, name='index')
+    path('movie/<int:pk>/details/', MovieDetail.as_view(), name='detail_movie'),
 ]
--- config/core/views.py
+++ config/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from core.models import Movie
+
 
 def testme(request):
     return render(request, 'base.html')
@@ -12,5 +13,11 @@ def all_movie(request):
 
 class MovieList(ListView):
     template_name = 'core/all_movies.html'
+    model = Movie
+    context_object_name = 'movies'
+
+
+class MovieDetail(DetailView):
+    template_name = 'core/detail_movie.html'
     model = Movie
     context_object_name = 'movies'
