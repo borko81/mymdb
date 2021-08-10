39f31e57d66d039d20494ac8172e5f9e675431c3
Borko <bstoilov81@gmail.com>
2021-08-10 14:41:33 +0300

    continue to edit procfile

--- config/config/settings.py
+++ config/config/settings.py
@@ -157,3 +157,6 @@ LOGOUT_URL = 'logout'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
