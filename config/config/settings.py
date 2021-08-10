aaaedd107752e25950b02756ca4adf39dd14fe4d
Borko <bstoilov81@gmail.com>
2021-08-10 13:37:32 +0300

    Change static_root

--- config/config/settings.py
+++ config/config/settings.py
@@ -125,7 +125,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
